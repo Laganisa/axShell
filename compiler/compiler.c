3376f8e79bdfda3b3be3de4d2dfab55b8396e785
Laganisa <1004skylake@gmail.com>
2026-05-25 01:10:26 +0900

    a0.0.1

--- compiler/compiler.c
+++
@@ -1,7 +0,0 @@
-#include "compiler.h"
-#include "shell.h"
-
-void compiler_run(void)
-{
-    shell_write("compiler module is ready.\n");
-}
--- include/editor.h
+++
@@ -1,6 +0,0 @@
-#ifndef axSHELL_EDITOR_H
-#define axSHELL_EDITOR_H
-
-void editor_run(void);
-
-#endif
